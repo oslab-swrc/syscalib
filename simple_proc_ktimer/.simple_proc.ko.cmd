43e72c33e3f6a8ad301c0f0e14b74241af870259
Changyu Lee <changyu.lee.02@gmail.com>
2021-11-03 14:26:44 +0900

    Delete simple_proc_ktimer directory

--- simple_proc_ktimer/.simple_proc.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ku-sslab/CorePartitioning/simple_proc_ktimer/simple_proc.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/ku-sslab/CorePartitioning/simple_proc_ktimer/simple_proc.ko /home/ku-sslab/CorePartitioning/simple_proc_ktimer/simple_proc.o /home/ku-sslab/CorePartitioning/simple_proc_ktimer/simple_proc.mod.o;  true
